ac47aeeee624ee45f1c8150c0fdf9bb86c071bfc
Igor <76621236+Cl1nt0n@users.noreply.github.com>
2021-03-06 00:50:55 +0300

    Add files via upload

---
+++ Paint25.02/Paint/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace Paint
+{
+    partial class paint
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(paint));
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            this.buttonLine = new System.Windows.Forms.Button();
+            this.buttonEllipse = new System.Windows.Forms.Button();
+            this.buttonErase = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarWight = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBoxFill = new System.Windows.Forms.CheckBox();
+            this.labelLineColor = new System.Windows.Forms.Label();
+            this.labelFillColor = new System.Windows.Forms.Label();
+            this.buttonText = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonPen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRectangle.BackgroundImage")));
+            this.buttonRectangle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRectangle.Location = new System.Drawing.Point(90, 27);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(75, 75);
+            this.buttonRectangle.TabIndex = 0;
+            this.buttonRectangle.UseVisualStyleBackColor = true;
+            this.buttonRectangle.Click += new System.EventHandler(this.buttonRectangle_Click);
+            // 
+            // buttonLine
+            // 
+            this.buttonLine.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLine.BackgroundImage")));
+            this.buttonLine.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLine.Location = new System.Drawing.Point(9, 27);
+            this.buttonLine.Name = "buttonLine";
+            this.buttonLine.Size = new System.Drawing.Size(75, 75);
+            this.buttonLine.TabIndex = 0;
+            this.buttonLine.UseVisualStyleBackColor = true;
+            this.buttonLine.Click += new System.EventHandler(this.buttonLine_Click);
+            // 
+            // buttonEllipse
+            // 
+            this.buttonEllipse.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonEllipse.BackgroundImage")));
+            this.buttonEllipse.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonEllipse.Location = new System.Drawing.Point(9, 108);
+            this.buttonEllipse.Name = "buttonEllipse";
+            this.buttonEllipse.Size = new System.Drawing.Size(75, 75);
+            this.buttonEllipse.TabIndex = 0;
+            this.buttonEllipse.UseVisualStyleBackColor = true;
+            this.buttonEllipse.Click += new System.EventHandler(this.buttonEllipse_Click);
+            // 
+            // buttonErase
+            // 
+            this.buttonErase.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonErase.BackgroundImage")));
+            this.buttonErase.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonErase.Location = new System.Drawing.Point(90, 108);
+            this.buttonErase.Name = "buttonErase";
+            this.buttonErase.Size = new System.Drawing.Size(37, 37);
+            this.buttonErase.TabIndex = 0;
+            this.buttonErase.UseVisualStyleBackColor = true;
+            this.buttonErase.Click += new System.EventHandler(this.buttonErase_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Толщина";
+            // 
+            // trackBarWight
+            // 
+            this.trackBarWight.Location = new System.Drawing.Point(12, 202);
+            this.trackBarWight.Maximum = 100;
+            this.trackBarWight.Minimum = 1;
+            this.trackBarWight.Name = "trackBarWight";
+            this.trackBarWight.Size = new System.Drawing.Size(153, 45);
+            this.trackBarWight.TabIndex = 2;
+            this.trackBarWight.Value = 1;
+            this.trackBarWight.Scroll += new System.EventHandler(this.trackBarWight_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Цвет контура";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Цвет заливки";
+            // 
+            // checkBoxFill
+            // 
+            this.checkBoxFill.AutoSize = true;
+            this.checkBoxFill.Location = new System.Drawing.Point(90, 240);
+            this.checkBoxFill.Name = "checkBoxFill";
+            this.checkBoxFill.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxFill.TabIndex = 3;
+            this.checkBoxFill.Text = "Заливка";
+            this.checkBoxFill.UseVisualStyleBackColor = true;
+            this.checkBoxFill.CheckedChanged += new System.EventHandler(this.checkBoxFill_CheckedChanged);
+            // 
+            // labelLineColor
+            // 
+            this.labelLineColor.AutoSize = true;
+            this.labelLineColor.BackColor = System.Drawing.Color.White;
+            this.labelLineColor.Location = new System.Drawing.Point(12, 274);
+            this.labelLineColor.MaximumSize = new System.Drawing.Size(25, 25);
+            this.labelLineColor.MinimumSize = new System.Drawing.Size(25, 25);
+            this.labelLineColor.Name = "labelLineColor";
+            this.labelLineColor.Size = new System.Drawing.Size(25, 25);
+            this.labelLineColor.TabIndex = 5;
+            this.labelLineColor.Click += new System.EventHandler(this.labelLineColor_Click);
+            // 
+            // labelFillColor
+            // 
+            this.labelFillColor.AutoSize = true;
+            this.labelFillColor.BackColor = System.Drawing.Color.White;
+            this.labelFillColor.Location = new System.Drawing.Point(87, 274);
+            this.labelFillColor.MaximumSize = new System.Drawing.Size(25, 25);
+            this.labelFillColor.MinimumSize = new System.Drawing.Size(25, 25);
+            this.labelFillColor.Name = "labelFillColor";
+            this.labelFillColor.Size = new System.Drawing.Size(25, 25);
+            this.labelFillColor.TabIndex = 6;
+            this.labelFillColor.Click += new System.EventHandler(this.labelFillColor_Click);
+            // 
+            // buttonText
+            // 
+            this.buttonText.BackColor = System.Drawing.Color.White;
+            this.buttonText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonText.Location = new System.Drawing.Point(90, 151);
+            this.buttonText.Name = "buttonText";
+            this.buttonText.Size = new System.Drawing.Size(75, 32);
+            this.buttonText.TabIndex = 7;
+            this.buttonText.Text = "Text";
+            this.buttonText.UseVisualStyleBackColor = false;
+            this.buttonText.Click += new System.EventHandler(this.buttonText_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox.Location = new System.Drawing.Point(171, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(960, 540);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 8;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1160, 24);
+            this.menuStrip.TabIndex = 10;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // buttonPen
+            // 
+            this.buttonPen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonPen.BackgroundImage")));
+            this.buttonPen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonPen.Location = new System.Drawing.Point(128, 108);
+            this.buttonPen.Name = "buttonPen";
+            this.buttonPen.Size = new System.Drawing.Size(37, 37);
+            this.buttonPen.TabIndex = 0;
+            this.buttonPen.UseVisualStyleBackColor = true;
+            this.buttonPen.Click += new System.EventHandler(this.buttonPen_Click);
+            // 
+            // paint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1160, 583);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonText);
+            this.Controls.Add(this.labelFillColor);
+            this.Controls.Add(this.labelLineColor);
+            this.Controls.Add(this.checkBoxFill);
+            this.Controls.Add(this.trackBarWight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPen);
+            this.Controls.Add(this.buttonErase);
+            this.Controls.Add(this.buttonEllipse);
+            this.Controls.Add(this.buttonLine);
+            this.Controls.Add(this.buttonRectangle);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "paint";
+            this.Text = "Paint 0.1";
+            this.Load += new System.EventHandler(this.paint_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarWight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRectangle;
+        private System.Windows.Forms.Button buttonLine;
+        private System.Windows.Forms.Button buttonEllipse;
+        private System.Windows.Forms.Button buttonErase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBarWight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBoxFill;
+        private System.Windows.Forms.Label labelLineColor;
+        private System.Windows.Forms.Label labelFillColor;
+        private System.Windows.Forms.Button buttonText;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.Button buttonPen;
+    }
+}
+
